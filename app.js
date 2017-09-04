cc24e815567aea8f3c8ea977559a43a28adafcef
leondo <leondo123@gmail.com>
2017-09-04 17:05:28 -0600

    fuck it im lazy

--- app.js
+++ app.js
@@ -1,152 +1,70 @@
+const Shapeshift = require('./lib/Shapeshift.js')
+const Bittrex = require('./lib/Bittrex.js')
 const fs = require('fs')
-require('es6-promise').polyfill()
-require('isomorphic-fetch')
-
 // ===========================
 
 // stores the previous data
 let previousData = {}
 
+const fallingKnife = async () => {
 
-const fallingKnife = () => {
+	// https://bittrex.com/api/v1.1/public/getmarketsummaries
+	const currentData = await Bittrex.fetchJSON()
+	if (currentData === false){
+		return fallingKnife()
+	}
 
-	// get bittrex data
-	fetch('https://bittrex.com/api/v1.1/public/getmarketsummaries')
+	// if there is no previous data, then set them =
+	if (Object.keys(previousData).length === 0){
+		previousData = currentData
+	}
 
-		// convert to json
-	    .then(response => {
-	        if (response.status >= 400) {
-	            throw new Error("Bad response from server")
-	        }
-	        return response.json()
-	    })
-
-	   	// find the falling knife 
-	    .then(currentData => {
-
-	    	// get the data
-	    	currentData = currentData.result
-
-	    	// the starting point
-	    	if (Object.keys(previousData).length === 0){
-	    		previousData = currentData
-	    	}
-
-	    	// this stores the greatest change
-	    	let greatestChange = 0
-
-	    	// compare currentData to previousData
-	        for (let i in currentData){
-
-	        	/*
-	        	 The magicical logic
-	        	 - if the current 'change' is more than the 'greatest change' and
-	        	 - current change is greater than ___
-	        	 - the base coin is BTC and
-	        	 - the 'exchange coin' is in the 'shapeshift coin' list
-	        	 - then update greatestChange
-	        	*/	        	
-	        	const currentChange = (Number(currentData[i].Last) - Number(previousData[i].Last)) / Number(previousData[i].Last)
-	    		const baseCoin = currentData[i].MarketName.split('-')[0]
-	    		const exchangeCoin = currentData[i].MarketName.split('-')[1]
-	    		//const shapeShiftCoins = ['BTC', '1ST', 'ANT', 'BAT', 'BNT', 'BCH', 'BCY', 'BLK', 'BTCD', 'BTS', 'CVC', 'CLAM', 'DASH', 'DCR', 'DGB', 'DGD', 'DNT', 'DOGE', 'EMC', 'EDG', 'EOS', 'ETH', 'ETC', 'FCT', 'FUN', 'GAME', 'GNO', 'GNT', 'GUP', 'ICN', 'KMD', 'LBC', 'LSK', 'LTC', 'MAID', 'MLN', 'MTL', 'MONA', 'MSC', 'NBT', 'NMC', 'NMR', 'NVC', 'NXT', 'OMG', 'PAY', 'POT', 'PPC', 'QTUM', 'REP', 'RDD', 'RLC', 'SC', 'SNT', 'SJCX', 'START', 'STEEM', 'SNGLS', 'SWT', 'TKN', 'TRST', 'USDT', 'VOX', 'VRC', 'VTC', 'WAVES', 'WINGS', 'XCP', 'XMR', 'XRP', 'ZEC', 'ZRX']
-	        	//const shapeShiftCoins =   [       '1ST', 'ANT', 'BAT', 'BNT', 'BCH'                             , 'CVC'        , 'DASH', 'DCR'       , 'DGD', 'DNT'                      , 'EOS', 'ETH'       , 'FCT', 'FUN'        , 'GNO', 'GNT'                                   , 'LTC'               , 'MTL'													 ,'OMG' 					, 'QTUM', 'REP'					   , 'SNT', 'SJCX'																						 , 'WINGS'							  , 'ZRX']
-				const shapeShiftCoins = ['1ST', 'ANT', 'REP', 'BAT', 'BNT', 'CVC', 'DGD', 'DNT', 'EOS', 'ETH', 'FUN', 'GNO', 'GNT', 'MTL', 'OMG', 'QTUM', 'SNT', 'WINGS', 'ZRX']
-
-	        	if (currentChange < greatestChange && 
-	        		currentChange < -0.001 &&
-	        		shapeShiftCoins.indexOf(exchangeCoin) != -1 &&
-	        		baseCoin === 'BTC'){
-		        		greatestChange = currentData[i]
-		        		greatestChange.knife = currentChange
-		        		greatestChange.coin = exchangeCoin
-	        	}
-
-	        	// update value of BTC-coin.txt
-	        	if (fs.readFileSync('coin.txt', 'utf8') === exchangeCoin &&
-	        		baseCoin === 'BTC'){
-	        			fs.writeFileSync('last.txt', currentData[i].Last, 'utf8')
-	        	}
-
-	        }
-
-	        // update previousData
-	        previousData = currentData
-
-	        // return greatestChange
-	        return greatestChange
-	    })
-
-	   	// trade here
-	    .then(recieveAccount => {
-
-			/*
-			recieveAccount = { 
-				MarketName: 'BTC-DOGE',
-				High: 5.1e-7,
-				Low: 4.7e-7,
-				Volume: 488940233.94318575,
-				Last: 4.7e-7,
-				BaseVolume: 238.90044034,
-				TimeStamp: '2017-09-03T15:12:05.79',
-				Bid: 4.7e-7,
-				Ask: 4.8e-7,
-				OpenBuyOrders: 1449,
-				OpenSellOrders: 8299,
-				PrevDay: 4.7e-7,
-				Created: '2014-02-13T00:00:00',
-				knife: -0.020833333333333245,
-				coin: 'DOGE' 
-			}
-			*/
-
-	    	// if there is not change, then skip the trade logic
-	    	if (recieveAccount === 0) {
-	    		return "not trading..."
-	    	}
-	    	
-	    	// figure out where my money is
-	    	let sendAccount = {
-				coin: 	fs.readFileSync('coin.txt', 'utf8'),
-				amount: fs.readFileSync('amount.txt', 'utf8'),
-				last : 	fs.readFileSync('last.txt', 'utf8'),
-			}
-
-			// new balance (Fancy math here)
-			const recieveAmount = Number(sendAccount.amount) * Number(sendAccount.last) / Number(recieveAccount.Last)
-
-			// transfer my money to the new coin
-			fs.writeFileSync('coin.txt', recieveAccount.coin, 'utf8')
-			fs.writeFileSync('amount.txt', recieveAmount, 'utf8')
-
-			// display message
-			return recieveAccount
+	// loop through to find the greatest change
+    let greatestChange = 0
+    // set the best coin as shapeshiftCoin
+    let shapeshiftCoin = {trade: false}
+    for (let i = 0; i < currentData.result.length; i++){
 
 
-	    })
+    	/*
+    	 The magicical logic
+    	 - if the current 'change' is more than the 'greatest change' and
+    	 - current change is greater than ___
+    	 - the base coin is BTC and
+    	 - the 'exchange coin' is in the 'shapeshift coin' list
+    	 - update the best coin to shapeshiftCoin
+    	*/	   
+    	const currentChange = (Number(currentData.result[i].Last) - Number(previousData.result[i].Last)) / Number(previousData.result[i].Last)
+		const baseCoin = currentData.result[i].MarketName.split('-')[0]
+		const exchangeCoin = currentData.result[i].MarketName.split('-')[1]
+		const shapeShiftCoins = ['1ST', 'ANT', 'REP', 'BAT', 'BNT', 'CVC', 'DGD', 'DNT', 'EOS', 'ETH', 'FUN', 'GNO', 'GNT', 'MTL', 'OMG', 'QTUM', 'SNT', 'WINGS', 'ZRX']
+    	
+    	// this stores the greatest change
+    	if (currentChange < greatestChange && 
+    		currentChange < -0.001 &&
+    		shapeShiftCoins.indexOf(exchangeCoin) != -1 &&
+    		baseCoin === 'BTC'){
+    			// update the greatstChange
+    			greatestChange = currentChange
+        		// update the best coin
+        		shapeshiftCoin = {
+        			trade: true,
+        			exchangeCoin: exchangeCoin,
+        			percent_change: greatestChange,
+        		}
+        }
 
-	    // display message
-	    .then((message) => {
+    }// for loop
 
-	    	message.coin = fs.readFileSync('coin.txt', 'utf8')
-	    	message.amount = fs.readFileSync('amount.txt', 'utf8')
-	    	
-	    	console.log('\n\n', message)
-
-	    	// set timeout then start over again
-	    	setTimeout(() => {
-	    		fallingKnife() 
-	    	}, 5000);
-	    })
+    if (shapeshiftCoin.trade === true){
+    	console.log(shapeshiftCoin)
+    }
 
 
-}// fallingKnife()
+    // set timeout then start over again
+	setTimeout(() => {
+		fallingKnife() 
+	}, 5000);
 
-
+}
 fallingKnife()
-
-
-
-
-
-
